5acc52a35b19e085aaaaf5692321eca7e04a9dcd
Raja Janarthanan <rajajanarthanan@gmail.com>
2025-01-15 17:01:19 +0530

    SimplesSvg_updatedPartially_optionsYettobeMapped

--- lib/core/data/simples_repository_impl.dart
+++ lib/core/data/simples_repository_impl.dart
@@ -1,3 +1,22 @@
+import 'package:dartz/dartz.dart';
+import 'package:get_it/get_it.dart';
 import 'package:simplyfirescore/core/domain/simples_repository.dart';
 
-class SimplesRepoSitoryImpl implements SimplesRepository {}
+import '../app/global_exports.dart';
+
+class SimplesRepoSitoryImpl implements SimplesRepository {
+  final ApiService _api = GetIt.I.get<ApiService>();
+
+  // Future<Either<SimplesError, SimplesResult<T>>> getApi<T>(
+  //     String endPoint, T Function(Map<String, dynamic> json) fromJson,
+  //     {Map<String, dynamic> queryParameters = const {},
+  //     Map<String, dynamic> headers = const {'Content-Type': 'application/json'},
+  //     String baseUrl = Config.simplesApiBaseUrl}) async {
+  //   final response = await _api.get(
+  //     baseUrl + endPoint,
+  //     fromJson,
+  //     queryParameters: queryParameters,
+  //     headers: headers,
+  //   );
+  // }
+}
